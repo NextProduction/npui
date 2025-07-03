26222da3ba4433a2169e0fe55da74565b5e1e37f
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2025-07-03 20:17:04 +0330

    clean old version dude

--- .storybook/manager.ts
+++
@@ -1,6 +0,0 @@
-import {addons} from "@storybook/manager-api"
-import npui_theme from "./npui_theme"
-
-addons.setConfig({
-    theme : npui_theme
-})
--- src/components/Table/Table.tsx
+++
@@ -1,5 +0,0 @@
-export default function  Table (){
-    return (
-        <div className="w-full">Table</div>
-    )
-}
